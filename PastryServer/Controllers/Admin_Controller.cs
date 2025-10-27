559843dbcc321caf3e0e8ebd320be8f5c4b1f8ee
Sprt6 I <sprt6i@proton.me>
2025-10-27 23:05:08 +0100

    Made Links Instead Of Coppies

---
+++ PastryServer/Controllers/Admin_Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PastryServer.Models;
+using PastryServer.Services;
+
+namespace PastryServer.Controllers
+{
+    [ApiController]
+    [Route("Admin")]
+    public class Admin_Controller : ControllerBase
+    {
+        private readonly Database_Service database;
+
+        public Admin_Controller(Database_Service database)
+        {
+            this.database = database;
+        }
+    }
+}
---
+++ PastryServer/Helper_Files/Checks.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PastryServer.Helper_Files
+{
+    public class Checks
+    {
+        public bool Is_Gmail_Valid_(string gmail)
+        {
+            if (string.IsNullOrWhiteSpace(gmail)) { return false; }
+
+            string pattern = @"^[a-zA-Z0-9._%+-]+@gmail\.com$";
+            if (!Regex.IsMatch(gmail, pattern, RegexOptions.IgnoreCase)) { return false; }
+
+            return true;
+        }
+
+        public bool Is_Password_Valid_(string password)
+        {
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{3,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+    }
+}
